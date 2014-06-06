100bce4d721e3d105db5046765e930c46565342f
idunn <idunn@8c20c411-d360-0410-9ebf-93e8b32640fb>
2014-06-06 15:48:18 +0000

    TT-3606: Updated deploy action to pass model to view
    git-svn-id: http://dubv-engsvn01.dubeng.local/svn/tools/ProductSetup/ProductSetupApp/trunk@9070 8c20c411-d360-0410-9ebf-93e8b32640fb

--- grails-app/controllers/hmof/ProgramController.groovy
+++ grails-app/controllers/hmof/ProgramController.groovy
@@ -10,117 +10,122 @@ import grails.transaction.Transactional
  */
 @Transactional(readOnly = true)
 class ProgramController {
-	
+
 	// inject Service
 	def deploymentService
 
-    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
-	
+	static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
 	/**
 	 * action to deploy content
 	 * @return
 	 */
 	def deploy() {
-		
+
 		def deploymentDetails = deploymentService.promoteProgram(params)
-		if (deploymentDetails.size()==0) render "Nothing to Deploy"
-		else render deploymentDetails
+		[deploymentDetailsList:deploymentDetails]
+		
+		/*if (deploymentDetails.size()==0) render "Nothing to Deploy"
+		else
+		{
+			[deploymentDetailsList:deploymentDetails]
+		}*/
 	}
 
 	def index(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
-        respond Program.list(params), model:[programInstanceCount: Program.count()]
-    }
+		params.max = Math.min(max ?: 10, 100)
+		respond Program.list(params), model:[programInstanceCount: Program.count()]
+	}
 
 	def list(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
-        respond Program.list(params), model:[programInstanceCount: Program.count()]
-    }
+		params.max = Math.min(max ?: 10, 100)
+		respond Program.list(params), model:[programInstanceCount: Program.count()]
+	}
 
-    def show(Program programInstance) {
-        respond programInstance
-    }
+	def show(Program programInstance) {
+		respond programInstance
+	}
 
-    def create() {
-        respond new Program(params)
-    }
+	def create() {
+		respond new Program(params)
+	}
 
-    @Transactional
-    def save(Program programInstance) {
-        if (programInstance == null) {
-            notFound()
-            return
-        }
+	@Transactional
+	def save(Program programInstance) {
+		if (programInstance == null) {
+			notFound()
+			return
+		}
 
-        if (programInstance.hasErrors()) {
-            respond programInstance.errors, view:'create'
-            return
-        }
+		if (programInstance.hasErrors()) {
+			respond programInstance.errors, view:'create'
+			return
+		}
 
-        programInstance.save flush:true
+		programInstance.save flush:true
 
-        request.withFormat {
-            form {
-                flash.message = message(code: 'default.created.message', args: [message(code: 'programInstance.label', default: 'Program'), programInstance.id])
-                redirect programInstance
-            }
-            '*' { respond programInstance, [status: CREATED] }
-        }
-    }
+		request.withFormat {
+			form {
+				flash.message = message(code: 'default.created.message', args: [message(code: 'programInstance.label', default: 'Program'), programInstance.id])
+				redirect programInstance
+			}
+			'*' { respond programInstance, [status: CREATED] }
+		}
+	}
 
-    def edit(Program programInstance) {
-        respond programInstance
-    }
+	def edit(Program programInstance) {
+		respond programInstance
+	}
 
-    @Transactional
-    def update(Program programInstance) {
-        if (programInstance == null) {
-            notFound()
-            return
-        }
+	@Transactional
+	def update(Program programInstance) {
+		if (programInstance == null) {
+			notFound()
+			return
+		}
 
-        if (programInstance.hasErrors()) {
-            respond programInstance.errors, view:'edit'
-            return
-        }
+		if (programInstance.hasErrors()) {
+			respond programInstance.errors, view:'edit'
+			return
+		}
 
-        programInstance.save flush:true
+		programInstance.save flush:true
 
-        request.withFormat {
-            form {
-                flash.message = message(code: 'default.updated.message', args: [message(code: 'Program.label', default: 'Program'), programInstance.id])
-                redirect programInstance
-            }
-            '*'{ respond programInstance, [status: OK] }
-        }
-    }
+		request.withFormat {
+			form {
+				flash.message = message(code: 'default.updated.message', args: [message(code: 'Program.label', default: 'Program'), programInstance.id])
+				redirect programInstance
+			}
+			'*'{ respond programInstance, [status: OK] }
+		}
+	}
 
-    @Transactional
-    def delete(Program programInstance) {
+	@Transactional
+	def delete(Program programInstance) {
 
-        if (programInstance == null) {
-            notFound()
-            return
-        }
+		if (programInstance == null) {
+			notFound()
+			return
+		}
 
-        programInstance.delete flush:true
+		programInstance.delete flush:true
 
-        request.withFormat {
-            form {
-                flash.message = message(code: 'default.deleted.message', args: [message(code: 'Program.label', default: 'Program'), programInstance.id])
-                redirect action:"index", method:"GET"
-            }
-            '*'{ render status: NO_CONTENT }
-        }
-    }
+		request.withFormat {
+			form {
+				flash.message = message(code: 'default.deleted.message', args: [message(code: 'Program.label', default: 'Program'), programInstance.id])
+				redirect action:"index", method:"GET"
+			}
+			'*'{ render status: NO_CONTENT }
+		}
+	}
 
-    protected void notFound() {
-        request.withFormat {
-            form {
-                flash.message = message(code: 'default.not.found.message', args: [message(code: 'programInstance.label', default: 'Program'), params.id])
-                redirect action: "index", method: "GET"
-            }
-            '*'{ render status: NOT_FOUND }
-        }
-    }
+	protected void notFound() {
+		request.withFormat {
+			form {
+				flash.message = message(code: 'default.not.found.message', args: [message(code: 'programInstance.label', default: 'Program'), params.id])
+				redirect action: "index", method: "GET"
+			}
+			'*'{ render status: NOT_FOUND }
+		}
+	}
 }
