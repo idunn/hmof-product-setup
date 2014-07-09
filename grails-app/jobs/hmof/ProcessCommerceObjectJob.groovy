4adc769889d4dfbe7b2eda701925cac3431b0b0f
idunn <idunn@8c20c411-d360-0410-9ebf-93e8b32640fb>
2014-07-09 10:29:11 +0000

    TT-3715: Added radio buttons with highlighting beside the selected instance - minor update to quartz
    git-svn-id: http://dubv-engsvn01.dubeng.local/svn/tools/ProductSetup/ProductSetupApp/trunk@9175 8c20c411-d360-0410-9ebf-93e8b32640fb

--- grails-app/jobs/hmof/ProcessCommerceObjectJob.groovy
+++ grails-app/jobs/hmof/ProcessCommerceObjectJob.groovy
@@ -4,11 +4,12 @@ package hmof
 
 class ProcessCommerceObjectJob {
     static triggers = {
-      simple repeatInterval: 30 * 1000 // execute job once in 30 seconds
+      //simple repeatInterval: 30 * 1000 // execute job once in 30 seconds
+		simple repeatInterval: 120 * 1000 // execute job once in 120 seconds - testing
     }
 
     def execute() {
         // execute job
-		println "Job Run!"
+		println "Quartz Job running!" + new Date()
     }
 }
