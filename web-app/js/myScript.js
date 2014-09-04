d2c2909307869a6bc479c8bd3ae92612b7e63e36
idunn <idunn@8c20c411-d360-0410-9ebf-93e8b32640fb>
2014-09-04 10:51:37 +0000

    TT-3715: JavaScript to help with radio button instance selection
    git-svn-id: http://dubv-engsvn01.dubeng.local/svn/tools/ProductSetup/ProductSetupApp/trunk@9639 8c20c411-d360-0410-9ebf-93e8b32640fb

---
+++ web-app/js/myScript.js
@@ -0,0 +1,72 @@
+function toggle(box,theId) {
+	//alert("inside myScript js toggle method");
+		  var el;
+		  if(document.getElementById) {
+		    var row = document.getElementById(theId);
+		    var f = row;
+		    while(f.tagName.toLowerCase() != 'form') {
+		      if(f.parentNode) f=f.parentNode;
+		      else if(f.parentElement) f=f.parentElement; //IE4 support
+		      if(f.tagName.toLowerCase() == 'body') {
+		        alert('ERROR: looped to find <'+'body'+'>'); return;
+		      }
+		    }
+		    for(var i=0;i<f.elements.length;i++) {
+		      if(f.elements[i].tagName.toLowerCase() == 'input')
+		        //the if condition above is to compensate for an NS6.0 bug.
+		        if(f.elements[i].type.toLowerCase() == 'radio')
+		          if(f.elements[i].name.toLowerCase().indexOf('rad')!=-1) {
+		            el=f.elements[i];
+		            while(el.tagName.toLowerCase() != 'tr') {
+		              if(el.parentNode) el=el.parentNode;
+		              else if(el.parentElement) el=el.parentElement; //IE4 support
+		              if(el.tagName.toLowerCase() == 'form') {
+		                alert('ERROR: looped to find <'+'form'+'>'); return;
+		              }
+		            }
+		          if(box != el) { el.className = "off"; }
+		          }
+		    }
+		    row.className = (box.checked)?"on":"off";
+		  }
+		}
+
+
+	function deploy(){				
+		if($("input:radio[name='rad']").is(":checked")){		
+		var optionSelected = $('input[name=rad]:checked').val();				
+		var instanceDetail = document.getElementById("instanceDetail");
+		instanceDetail.value = optionSelected;		
+		var deployProgram=confirm("Are you sure you want to deploy this program?");
+		if(deployProgram){
+			return true;
+			}
+		else{
+			return false; 
+			}		
+		}
+		else{
+				alert("Please select a program to deploy!");
+				return false;
+			}
+	}
+
+
+	function promote(){				
+		if($("input:radio[name='rad']").is(":checked")){		
+		var optionSelected = $('input[name=rad]:checked').val();				
+		var instanceToBePromoted = document.getElementById("instanceToBePromoted");
+		instanceToBePromoted.value = optionSelected;		
+		var promoteProgram=confirm("Are you sure you want to promote this program?");
+		if(promoteProgram){
+			return true;
+			}
+		else{
+			return false; 
+			}		
+		}
+		else{
+				alert("Please select a program to promote!");
+				return false;
+			}
+	}
