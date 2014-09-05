0a6a8c44bd8eb323c656164f88a4967f60676fc4
idunn <idunn@8c20c411-d360-0410-9ebf-93e8b32640fb>
2014-09-05 13:11:42 +0000

    TT-3715: Process Job and Quartz
    git-svn-id: http://dubv-engsvn01.dubeng.local/svn/tools/ProductSetup/ProductSetupApp/trunk@9677 8c20c411-d360-0410-9ebf-93e8b32640fb

--- grails-app/jobs/hmof/ProcessJob.groovy
+++ grails-app/jobs/hmof/ProcessJob.groovy
@@ -1,15 +1,27 @@
 package hmof
 
+import hmh.poc.deploy.Promotion
+import hmh.poc.deploy.Job
+
 
 
 class ProcessCommerceObjectJob {
-    static triggers = {
-      //simple repeatInterval: 30 * 1000 // execute job once in 30 seconds
-		simple repeatInterval: 120 * 1000 // execute job once in 120 seconds - testing
-    }
 
-    def execute() {
-        // execute job
-		println "Quartz Job running!" + new Date()
-    }
+	def deploymentService
+
+	static triggers = {
+		//simple repeatInterval: 30 * 1000 // execute job once in 30 seconds
+		simple repeatInterval: 120 * 1000 // execute job once in 120 seconds - testing
+	}
+
+	def execute() {
+		def result = []
+		// execute job
+		println "Inside Job"
+		result = deploymentService.executeJob()
+		if(result !=null){
+			println "Results of deployment/promotion: " + result
+			deploymentService.updateStatus(result)
+		}else{ println 'Nothing to deploy/promote'}
+	}
 }
