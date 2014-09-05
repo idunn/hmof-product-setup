39da59b48e2f37577f14a5289c0af46dcd078834
idunn <idunn@8c20c411-d360-0410-9ebf-93e8b32640fb>
2014-09-05 10:03:42 +0000

    TT-3715: Updated SP Controller
    git-svn-id: http://dubv-engsvn01.dubeng.local/svn/tools/ProductSetup/ProductSetupApp/trunk@9654 8c20c411-d360-0410-9ebf-93e8b32640fb

--- grails-app/controllers/hmof/SecureProgramController.groovy
+++ grails-app/controllers/hmof/SecureProgramController.groovy
@@ -11,116 +11,102 @@ import grails.transaction.Transactional
 @Transactional(readOnly = true)
 class SecureProgramController {
 
-	// inject Service
-	def deploymentService
-
-	static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
-
-	def deploy() {
-
-		def (secureProgram, commerceObjects) = deploymentService.promoteSecureProgram(params)
-
-		if (secureProgram.size() + commerceObjects.size()==0) render "Nothing to Deploy"
-		else
-		{
-			[secureProgramInstance:secureProgram, commerceObjectsList:commerceObjects]
-		}
-	}
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
 	def index(Integer max) {
-		params.max = Math.min(max ?: 10, 100)
-		respond SecureProgram.list(params), model:[secureProgramInstanceCount: SecureProgram.count()]
-	}
+        params.max = Math.min(max ?: 10, 100)
+        respond SecureProgram.list(params), model:[secureProgramInstanceCount: SecureProgram.count()]
+    }
 
 	def list(Integer max) {
-		params.max = Math.min(max ?: 10, 100)
-		respond SecureProgram.list(params), model:[secureProgramInstanceCount: SecureProgram.count()]
-	}
+        params.max = Math.min(max ?: 10, 100)
+        respond SecureProgram.list(params), model:[secureProgramInstanceCount: SecureProgram.count()]
+    }
 
-	def show(SecureProgram secureProgramInstance) {
-		respond secureProgramInstance
-	}
+    def show(SecureProgram secureProgramInstance) {
+        respond secureProgramInstance
+    }
 
-	def create() {
-		respond new SecureProgram(params)
-	}
+    def create() {
+        respond new SecureProgram(params)
+    }
 
-	@Transactional
-	def save(SecureProgram secureProgramInstance) {
-		if (secureProgramInstance == null) {
-			notFound()
-			return
-		}
+    @Transactional
+    def save(SecureProgram secureProgramInstance) {
+        if (secureProgramInstance == null) {
+            notFound()
+            return
+        }
 
-		if (secureProgramInstance.hasErrors()) {
-			respond secureProgramInstance.errors, view:'create'
-			return
-		}
+        if (secureProgramInstance.hasErrors()) {
+            respond secureProgramInstance.errors, view:'create'
+            return
+        }
 
-		secureProgramInstance.save flush:true
+        secureProgramInstance.save flush:true
 
-		request.withFormat {
-			form {
-				flash.message = message(code: 'default.created.message', args: [message(code: 'secureProgramInstance.label', default: 'SecureProgram'), secureProgramInstance.id])
-				redirect secureProgramInstance
-			}
-			'*' { respond secureProgramInstance, [status: CREATED] }
-		}
-	}
+        request.withFormat {
+            form {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'secureProgramInstance.label', default: 'SecureProgram'), secureProgramInstance.id])
+                redirect secureProgramInstance
+            }
+            '*' { respond secureProgramInstance, [status: CREATED] }
+        }
+    }
 
-	def edit(SecureProgram secureProgramInstance) {
-		respond secureProgramInstance
-	}
+    def edit(SecureProgram secureProgramInstance) {
+        respond secureProgramInstance
+    }
 
-	@Transactional
-	def update(SecureProgram secureProgramInstance) {
-		if (secureProgramInstance == null) {
-			notFound()
-			return
-		}
+    @Transactional
+    def update(SecureProgram secureProgramInstance) {
+        if (secureProgramInstance == null) {
+            notFound()
+            return
+        }
 
-		if (secureProgramInstance.hasErrors()) {
-			respond secureProgramInstance.errors, view:'edit'
-			return
-		}
+        if (secureProgramInstance.hasErrors()) {
+            respond secureProgramInstance.errors, view:'edit'
+            return
+        }
 
-		secureProgramInstance.save flush:true
+        secureProgramInstance.save flush:true
 
-		request.withFormat {
-			form {
-				flash.message = message(code: 'default.updated.message', args: [message(code: 'SecureProgram.label', default: 'SecureProgram'), secureProgramInstance.id])
-				redirect secureProgramInstance
-			}
-			'*'{ respond secureProgramInstance, [status: OK] }
-		}
-	}
+        request.withFormat {
+            form {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'SecureProgram.label', default: 'SecureProgram'), secureProgramInstance.id])
+                redirect secureProgramInstance
+            }
+            '*'{ respond secureProgramInstance, [status: OK] }
+        }
+    }
 
-	@Transactional
-	def delete(SecureProgram secureProgramInstance) {
+    @Transactional
+    def delete(SecureProgram secureProgramInstance) {
 
-		if (secureProgramInstance == null) {
-			notFound()
-			return
-		}
+        if (secureProgramInstance == null) {
+            notFound()
+            return
+        }
 
-		secureProgramInstance.delete flush:true
+        secureProgramInstance.delete flush:true
 
-		request.withFormat {
-			form {
-				flash.message = message(code: 'default.deleted.message', args: [message(code: 'SecureProgram.label', default: 'SecureProgram'), secureProgramInstance.id])
-				redirect action:"index", method:"GET"
-			}
-			'*'{ render status: NO_CONTENT }
-		}
-	}
+        request.withFormat {
+            form {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'SecureProgram.label', default: 'SecureProgram'), secureProgramInstance.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
 
-	protected void notFound() {
-		request.withFormat {
-			form {
-				flash.message = message(code: 'default.not.found.message', args: [message(code: 'secureProgramInstance.label', default: 'SecureProgram'), params.id])
-				redirect action: "index", method: "GET"
-			}
-			'*'{ render status: NOT_FOUND }
-		}
-	}
+    protected void notFound() {
+        request.withFormat {
+            form {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'secureProgramInstance.label', default: 'SecureProgram'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
 }
