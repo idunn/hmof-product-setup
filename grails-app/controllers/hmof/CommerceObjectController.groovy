c895148f5a757cc271e934294b0b7a350d4d14d0
idunn <idunn@8c20c411-d360-0410-9ebf-93e8b32640fb>
2014-09-05 10:03:02 +0000

    TT-3715: Updated CO Controller
    git-svn-id: http://dubv-engsvn01.dubeng.local/svn/tools/ProductSetup/ProductSetupApp/trunk@9653 8c20c411-d360-0410-9ebf-93e8b32640fb

--- grails-app/controllers/hmof/CommerceObjectController.groovy
+++ grails-app/controllers/hmof/CommerceObjectController.groovy
@@ -11,125 +11,102 @@ import grails.transaction.Transactional
 @Transactional(readOnly = true)
 class CommerceObjectController {
 
-	def deploymentService
-	def gebService
-
-	static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
-
-	@Transactional
-	def deploy() {
-
-		def (commerceObject) = deploymentService.promoteCommerceObject(params)
-
-		if (commerceObject.size()==0) render "Nothing to Deploy"
-		else
-		{
-			// Pass to Geb - possibly return true
-			def deploymentJob = gebService.deployCommerceObject(commerceObject)		
-			
-			// pass control back to index
-			redirect(action: 'index')
-			//chain action: 'index'
-			
-			// send detached criteria to view
-			//[commerceObjectInstance:commerceObject]
-		}
-	}
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
 	def index(Integer max) {
-		// This was the same as the list action
-		redirect(action: "list", params: params)
-	}
+        params.max = Math.min(max ?: 10, 100)
+        respond CommerceObject.list(params), model:[commerceObjectInstanceCount: CommerceObject.count()]
+    }
 
 	def list(Integer max) {
-		params.max = Math.min(max ?: 10, 100)
-		respond CommerceObject.list(params), model:[commerceObjectInstanceCount: CommerceObject.count()]
-	}
+        params.max = Math.min(max ?: 10, 100)
+        respond CommerceObject.list(params), model:[commerceObjectInstanceCount: CommerceObject.count()]
+    }
 
-	def show(CommerceObject commerceObjectInstance) {
-		respond commerceObjectInstance
-	}
+    def show(CommerceObject commerceObjectInstance) {
+        respond commerceObjectInstance
+    }
 
-	def create() {
-		respond new CommerceObject(params)
-	}
+    def create() {
+        respond new CommerceObject(params)
+    }
 
-	@Transactional
-	def save(CommerceObject commerceObjectInstance) {
-		if (commerceObjectInstance == null) {
-			notFound()
-			return
-		}
+    @Transactional
+    def save(CommerceObject commerceObjectInstance) {
+        if (commerceObjectInstance == null) {
+            notFound()
+            return
+        }
 
-		if (commerceObjectInstance.hasErrors()) {
-			respond commerceObjectInstance.errors, view:'create'
-			return
-		}
+        if (commerceObjectInstance.hasErrors()) {
+            respond commerceObjectInstance.errors, view:'create'
+            return
+        }
 
-		commerceObjectInstance.save flush:true
+        commerceObjectInstance.save flush:true
 
-		request.withFormat {
-			form {
-				flash.message = message(code: 'default.created.message', args: [message(code: 'commerceObjectInstance.label', default: 'CommerceObject'), commerceObjectInstance.id])
-				redirect commerceObjectInstance
-			}
-			'*' { respond commerceObjectInstance, [status: CREATED] }
-		}
-	}
+        request.withFormat {
+            form {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'commerceObjectInstance.label', default: 'CommerceObject'), commerceObjectInstance.id])
+                redirect commerceObjectInstance
+            }
+            '*' { respond commerceObjectInstance, [status: CREATED] }
+        }
+    }
 
-	def edit(CommerceObject commerceObjectInstance) {
-		respond commerceObjectInstance
-	}
+    def edit(CommerceObject commerceObjectInstance) {
+        respond commerceObjectInstance
+    }
 
-	@Transactional
-	def update(CommerceObject commerceObjectInstance) {
-		if (commerceObjectInstance == null) {
-			notFound()
-			return
-		}
+    @Transactional
+    def update(CommerceObject commerceObjectInstance) {
+        if (commerceObjectInstance == null) {
+            notFound()
+            return
+        }
 
-		if (commerceObjectInstance.hasErrors()) {
-			respond commerceObjectInstance.errors, view:'edit'
-			return
-		}
+        if (commerceObjectInstance.hasErrors()) {
+            respond commerceObjectInstance.errors, view:'edit'
+            return
+        }
 
-		commerceObjectInstance.save flush:true
+        commerceObjectInstance.save flush:true
 
-		request.withFormat {
-			form {
-				flash.message = message(code: 'default.updated.message', args: [message(code: 'CommerceObject.label', default: 'CommerceObject'), commerceObjectInstance.id])
-				redirect commerceObjectInstance
-			}
-			'*'{ respond commerceObjectInstance, [status: OK] }
-		}
-	}
+        request.withFormat {
+            form {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'CommerceObject.label', default: 'CommerceObject'), commerceObjectInstance.id])
+                redirect commerceObjectInstance
+            }
+            '*'{ respond commerceObjectInstance, [status: OK] }
+        }
+    }
 
-	@Transactional
-	def delete(CommerceObject commerceObjectInstance) {
+    @Transactional
+    def delete(CommerceObject commerceObjectInstance) {
 
-		if (commerceObjectInstance == null) {
-			notFound()
-			return
-		}
+        if (commerceObjectInstance == null) {
+            notFound()
+            return
+        }
 
-		commerceObjectInstance.delete flush:true
+        commerceObjectInstance.delete flush:true
 
-		request.withFormat {
-			form {
-				flash.message = message(code: 'default.deleted.message', args: [message(code: 'CommerceObject.label', default: 'CommerceObject'), commerceObjectInstance.id])
-				redirect action:"index", method:"GET"
-			}
-			'*'{ render status: NO_CONTENT }
-		}
-	}
+        request.withFormat {
+            form {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'CommerceObject.label', default: 'CommerceObject'), commerceObjectInstance.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
 
-	protected void notFound() {
-		request.withFormat {
-			form {
-				flash.message = message(code: 'default.not.found.message', args: [message(code: 'commerceObjectInstance.label', default: 'CommerceObject'), params.id])
-				redirect action: "index", method: "GET"
-			}
-			'*'{ render status: NOT_FOUND }
-		}
-	}
+    protected void notFound() {
+        request.withFormat {
+            form {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'commerceObjectInstance.label', default: 'CommerceObject'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
 }
