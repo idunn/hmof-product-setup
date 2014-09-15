2c4bc8d95a287a22f6b7edd936f7a7f044afa165
idunn <idunn@8c20c411-d360-0410-9ebf-93e8b32640fb>
2014-09-15 10:25:22 +0000

    TT-3871: Updated Geb Configuration
    git-svn-id: http://dubv-engsvn01.dubeng.local/svn/tools/ProductSetup/ProductSetupApp/trunk@9771 8c20c411-d360-0410-9ebf-93e8b32640fb

--- grails-app/conf/GebConfig.groovy
+++ grails-app/conf/GebConfig.groovy
@@ -1,8 +1,25 @@
+import org.openqa.selenium.WebDriver
 import org.openqa.selenium.phantomjs.PhantomJSDriver
 import org.openqa.selenium.remote.DesiredCapabilities
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions
 
-//System.setProperty('phantomjs.binary.path', 'C:\\Java\\phantomjs.exe')
-//driver = {new PhantomJSDriver(new DesiredCapabilities())}
+System.setProperty('phantomjs.binary.path', 'C:\\Java\\phantomjs.exe')
+driver = {new PhantomJSDriver(new DesiredCapabilities())}
+
+/*File file = new File("C:/Java/chrome/chromedriver.exe")
+System.setProperty('webdriver.chrome.driver', file.getAbsolutePath())
+DesiredCapabilities capabilities = DesiredCapabilities.chrome()
+capabilities.setCapability("chrome.switches", Arrays.asList("--incognito"))
+//WebDriver driver = new ChromeDriver()
+ChromeOptions options = new ChromeOptions()
+options.addArguments("--test-type")
+capabilities.setCapability("chrome.binary",file.getAbsolutePath())
+capabilities.setCapability(ChromeOptions.CAPABILITY, options)
+driver = new ChromeDriver(capabilities)
+//WebDriver driver = new ChromeDriver(options)
+*/
+
 reportsDir = "target/geb-reports"
 
 waiting {
