452ffae06ecadb1bdd7197b0e65bc2b54be72409
idunn <idunn@8c20c411-d360-0410-9ebf-93e8b32640fb>
2014-10-09 15:10:01 +0000

    TT-3982: Turned off concurrency
    git-svn-id: http://dubv-engsvn01.dubeng.local/svn/tools/ProductSetup/ProductSetupApp/trunk@9951 8c20c411-d360-0410-9ebf-93e8b32640fb

--- grails-app/jobs/hmof/ProcessJob.groovy
+++ grails-app/jobs/hmof/ProcessJob.groovy
@@ -1,21 +1,23 @@
 package hmof
 
 class ProcessJob {
+	
+	def concurrent = false
 
-	def deploymentService	
+	def deploymentService
 
 	static triggers = {
-		simple repeatInterval: 30 * 1000 // execute job once in 30 seconds
-		//simple repeatInterval: 120 * 1000 // execute job once in 120 seconds - testing
+		simple repeatInterval: 30 * 1000 // execute job once in 30 seconds		
+		
 	}
 
 	def execute() {
 		def result = []
-		// execute job		
+		// execute job
 		result = deploymentService.executeJob()
 		if(result !=null){
 			println "Results of deployment/promotion: " + result
 			deploymentService.updateStatus(result)
-		}//else{ println 'Nothing to deploy/promote'}
+		}
 	}
 }
