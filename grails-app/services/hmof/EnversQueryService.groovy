285da46f9274320f24e43969b6147798c19708be
idunn <idunn@8c20c411-d360-0410-9ebf-93e8b32640fb>
2014-10-17 11:34:09 +0000

    TT-3982: updated sql query to support mysql and limit the result to 1 row
    git-svn-id: http://dubv-engsvn01.dubeng.local/svn/tools/ProductSetup/ProductSetupApp/trunk@10003 8c20c411-d360-0410-9ebf-93e8b32640fb

--- grails-app/services/hmof/EnversQueryService.groovy
+++ grails-app/services/hmof/EnversQueryService.groovy
@@ -12,26 +12,26 @@ class EnversQueryService {
 
 	def getDeletedObject(instanceId, revision, contentType){
 
-		log.info "instanceId: " +  instanceId
-		log.info "revision: " +  revision
-		log.info "contentType: " +  contentType
+		log.info "Deleted Objects instanceId: " +  instanceId
+		log.info "Deleted Objects revision: " +  revision
+		log.info "Deleted Objects contentType: " +  contentType
 		
 		def sql = new Sql(dataSource)
 
 		def row = null 
 		
 		if (contentType==2){
-			row = sql.firstRow( "select * from Bundle_Aud where id=? AND REVTYPE !=2 AND rev=?", [instanceId, revision] )
+			row = sql.firstRow( "select * from Bundle_Aud where id=? AND REVTYPE !=2 AND rev=? Order By LAST_UPDATED desc LIMIT 1", [instanceId, revision] )
 			
 		}
 		
 		if (contentType==3){
-			row = sql.firstRow( "select * from SECURE_PROGRAM_AUD where id=? AND REVTYPE !=2 AND rev=?", [instanceId, revision] )
+			row = sql.firstRow( "select * from SECURE_PROGRAM_AUD where id=? AND REVTYPE !=2 AND rev=? Order By LAST_UPDATED desc LIMIT 1", [instanceId, revision] )
 			
 		}
 		
 		if (contentType==4){
-			row = sql.firstRow( "select * from COMMERCE_OBJECT_AUD where id=? AND REVTYPE !=2 AND rev=?", [instanceId, revision] )
+			row = sql.firstRow( "select * from COMMERCE_OBJECT_AUD where id=? AND REVTYPE !=2 AND rev=? Order By LAST_UPDATED desc LIMIT 1", [instanceId, revision] )
 			
 		}		
 		
