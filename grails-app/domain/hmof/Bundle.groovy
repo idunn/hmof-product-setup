2a1d3b3625f848912bb5a2fe13f0ebdb1b2e45d4
idunn <idunn@8c20c411-d360-0410-9ebf-93e8b32640fb>
2014-10-23 10:08:06 +0000

    TT-4074: static mapping to reorder content types in descending order by ID
    git-svn-id: http://dubv-engsvn01.dubeng.local/svn/tools/ProductSetup/ProductSetupApp/trunk@10034 8c20c411-d360-0410-9ebf-93e8b32640fb

--- grails-app/domain/hmof/Bundle.groovy
+++ grails-app/domain/hmof/Bundle.groovy
@@ -14,13 +14,17 @@ class Bundle {
 	Date lastUpdated
 	Boolean includePremiumCommerceObjects
 	ContentType contentType
-	//Program program
 	static belongsTo = [program:Program]
 	static hasMany = [secureProgram:SecureProgram]
 	static searchable = {
 		secureProgram component: true
-		   
-   }
+
+	}
+
+	static mapping = {
+
+		sort id: "desc"
+	}
 	static constraints = {
 
 		isbn (blank: false, matches:/([0-9]{13})$/,unique: true)
@@ -28,7 +32,7 @@ class Bundle {
 		includePremiumCommerceObjects(nullable: true)
 		duration (inList: ["1-Year", "2-Year", "3-Year", "4-Year", "5-Year", "6-Year", "7-Year", "8-Year"], nullable:true)
 		program()
-		}
+	}
 
 	String toString(){
 		"${isbn}: ${title}"
--- grails-app/domain/hmof/Program.groovy
+++ grails-app/domain/hmof/Program.groovy
@@ -5,26 +5,32 @@ import org.hibernate.envers.Audited
 
 @Audited
 class Program {
-	//static searchable = true
+
 	String name
 	String discipline
 	Date dateCreated
 	Date lastUpdated
-	
+
 	ContentType contentType
-	
+
 	static hasMany = [bundles:Bundle]
-	static searchable = {		
-			 bundles component: true
-				
+	static searchable = {
+		bundles component: true
+
 	}
+
+	static mapping = {
+
+		sort id: "desc"
+	}
+
 	static constraints = {
 
-		name (matches: /[A-Za-z0-9]+/, nullable:false, unique:true, blank:false)
+		name (matches: /[A-Za-z0-9]+/, nullable:false, unique:false, blank:false)
 		discipline(inList: ['language_arts', 'math', 'world_languages', 'social_studies', 'Other'], nullable:false, blank:false)
 		contentType ()
 		dateCreated ()
-		lastUpdated ()	
+		lastUpdated ()
 		bundles()
 	}
 
