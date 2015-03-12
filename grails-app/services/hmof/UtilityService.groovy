84af3590060f7f79a86575499fd08d3b16a8412b
arollapati <arollapati@8c20c411-d360-0410-9ebf-93e8b32640fb>
2015-03-12 06:02:57 +0000

    TT-4419: Changed the copy right symbol in csv SP's import
    git-svn-id: http://dubv-engsvn01.dubeng.local/svn/tools/ProductSetup/ProductSetupApp/trunk@10597 8c20c411-d360-0410-9ebf-93e8b32640fb

--- grails-app/services/hmof/UtilityService.groovy
+++ grails-app/services/hmof/UtilityService.groovy
@@ -241,7 +241,7 @@ class UtilityService {
 					}
 
 
-					SecureProgram dom=new SecureProgram (productName:tokens[0], registrationIsbn:tokens[1].replaceAll('"',''),comments:tokens[2],
+					SecureProgram dom=new SecureProgram (productName:tokens[0].replaceAll('&#169;', '©'), registrationIsbn:tokens[1].replaceAll('"',''),comments:tokens[2],
 					onlineIsbn:tokens[3].replaceAll('"',''),curriculumArea:tokens[4], copyright:tokens[5],labelForOnlineResource:tokens[6],pathToResource:tokens[7],essayGraderPrompts:tokens[8], pathToCoverImage:tokens[9],
 					knewtonProduct:knewtonProductVal,knowledgeGraphIdProd:tokens[10],knowledgeGraphWarmUpTimeLimit:tokens[11],knowledgeGraphEnrichmentTimeLimit:tokens[12],knowledgeGraphEnrichmentCbiTimeLimit:tokens[13],
 					labelForTeacherAdditionalResource:tokens[14],pathToTeacherAdditionalResource:tokens[15],
