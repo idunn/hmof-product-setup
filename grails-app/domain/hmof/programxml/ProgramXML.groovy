609667b9047be1cda9dcee084ba18f290367ab89
idunn <idunn@8c20c411-d360-0410-9ebf-93e8b32640fb>
2015-11-16 10:42:57 +0000

    TT-5123: override toString to return a meaningful value
    git-svn-id: http://dubv-engsvn01.dubeng.local/svn/tools/ProductSetup/HMOFProductSetupApp/trunk@11872 8c20c411-d360-0410-9ebf-93e8b32640fb

--- grails-app/domain/hmof/programxml/ProgramXML.groovy
+++ grails-app/domain/hmof/programxml/ProgramXML.groovy
@@ -15,15 +15,15 @@ class ProgramXML implements Comparable{
 	String language
 	String filename
 	String userUpdatingProgramXML
-	
-	
+
+
 	Date dateCreated
 	Date lastUpdated
 
 	SortedSet secureProgram
 	static hasMany = [secureProgram:SecureProgram]
 	ContentType contentType
-	
+
 	static mapping = {
 		sort id: "desc"
 	}
@@ -32,36 +32,39 @@ class ProgramXML implements Comparable{
 	}
 	static constraints = {
 		title (nullable:false,blank: false)
-		
+
 		dateCreated ()
 		lastUpdated ()
 		buid (validator: { val, obj ->
 			if(val==null)
 			{
 
-					return 'hmof.programxml.ProgramXML.buid.nullable'
-				
+				return 'hmof.programxml.ProgramXML.buid.nullable'
+
 			}else
 			{
 				return true
 			}
-			
+
 		},nullable:false,blank: false,unique: true)
 		language (inList: ["en_US","en_AS","en_AU","en_BE","en_BZ","en_BW","en_CA","en_GU","en_HK","en_IN","en_IE","en_JM","en_MT","en_MH","en_MU","en_NA","en_NZ","en_MP","en_PK","en_PH","en_SG","en_ZA","en_TT","en_UM","en_VI","en_GB","en_ZW"], nullable:false)
 		filename(validator: { val, obj ->
 			if(val==null)
 			{
 
-					return 'hmof.programxml.ProgramXML.filename.nullable'
-				
+				return 'hmof.programxml.ProgramXML.filename.nullable'
+
 			}else
 			{
 				return true
 			}
 		},matches:/^\S+\.\S+$/,nullable:false,blank: false,unique: true)
 	}
+	
+	
 	String toString(){
 		
+		return id
 	}
-	
+
 }
