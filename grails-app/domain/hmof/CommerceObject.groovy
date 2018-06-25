b534f7a48b32ed0cbd69d016b887b8eea3c35376
idunn <idunn@8c20c411-d360-0410-9ebf-93e8b32640fb>
2018-06-25 15:21:10 +0000

    TT-1234: Added new Object Type - Writable to inList values
    git-svn-id: http://dubv-engsvn01.dubeng.local/svn/tools/ProductSetup/HMOFProductSetupApp/trunk@15864 8c20c411-d360-0410-9ebf-93e8b32640fb

--- grails-app/domain/hmof/CommerceObject.groovy
+++ grails-app/domain/hmof/CommerceObject.groovy
@@ -85,7 +85,7 @@ class CommerceObject implements Comparable{
 				return true
 			}
 		})
-		objectType (inList: ['Other','DLO', 'eBook','myWriteSmart Activities', 'FYI', 'IWB', 'Notebook', 'Professional Development', 'Resources', 'Tab', 'ePlanner'])
+		objectType (inList: ['Other','DLO', 'eBook','myWriteSmart Activities', 'FYI', 'IWB', 'Notebook', 'Professional Development', 'Resources', 'Tab', 'ePlanner', 'Writable'])
 		objectReorderNumber (inList: ["0","1","2","3","4",'5','6','7','8','9','10','11','12','13','14','15','16',"Hidden"]) // only legacy products can have order number 0
 		gradeLevel (inList: ['K','1','2','3','4','5','6' ,'7' , '8', '9', '10', '11', '12', '6-8', '9-12', '6-12' ] )
 		comments (maxSize:200, nullable:true, widget: 'textarea')
